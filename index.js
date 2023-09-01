a4a7103fd88649a16ea13b804620d7ad1af9842b
Raghav2806 <142433224+Raghav2806@users.noreply.github.com>
2023-09-01 15:20:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$("button").mouseenter(function() {
+    $("button").css("backgroundColor","black");
+});
+$("button").mouseleave(function() {
+    $("button").css("backgroundColor","hsl(5, 85%, 63%)");
+});
+
+
